ac98520259bfd1d0de404a1c85fc372a9b110dcf
frizbog1 <frizbog1@9410121a-2b92-11de-a3db-a5b3f7bb893a>
2009-04-20 01:19:52 +0000

    Added initial ANT script and added license comments to all source files, as well as to the jar file created by the ANT script.

--- gedcom4j/src/com/mattharrah/gedcom4j/parser/LinePieces.java
+++ gedcom4j/src/com/mattharrah/gedcom4j/parser/LinePieces.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2009 Matthew R. Harrah
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.mattharrah.gedcom4j.parser;
 
 public class LinePieces {
@@ -5,20 +29,20 @@ public class LinePieces {
 	public String id;
 	public String tag;
 	public String remainder;
-	
+
 	public LinePieces(String line) {
 		String[] parts = line.split(" ");
 		level = Integer.parseInt(parts[0]);
-		if (parts.length==2) {
+		if (parts.length == 2) {
 			tag = parts[1];
-		} else if (parts.length>=3) {
+		} else if (parts.length >= 3) {
 			if (parts[1].startsWith("@") && parts[1].endsWith("@")) {
 				tag = parts[2];
 				id = parts[1];
-				remainder = joinParts(parts,3);
+				remainder = joinParts(parts, 3);
 			} else {
 				tag = parts[1];
-				remainder = joinParts(parts,2);
+				remainder = joinParts(parts, 2);
 			}
 		}
 	}
@@ -27,7 +51,7 @@ public class LinePieces {
 		StringBuilder sb = new StringBuilder();
 		for (int i = startFrom; i < parts.length; i++) {
 			sb.append(parts[i]);
-			if (i < parts.length-1) {
+			if (i < parts.length - 1) {
 				sb.append(" ");
 			}
 		}
