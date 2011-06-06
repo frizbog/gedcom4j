5c8567bc5d5f209bf0f909fe036d765561a6b9af
frizbog1@gmail.com <frizbog1@gmail.com@9410121a-2b92-11de-a3db-a5b3f7bb893a>
2011-06-06 00:48:28 +0000

    Added new comparator for sorting lists of individuals by last name, then first name

---
+++ gedcom4j/src/com/mattharrah/gedcom4j/comparators/IndividualByLastNameFirstNameComparator.java
@@ -0,0 +1,25 @@
+package com.mattharrah.gedcom4j.comparators;
+
+import java.util.Comparator;
+
+import com.mattharrah.gedcom4j.Individual;
+
+public class IndividualByLastNameFirstNameComparator implements
+        Comparator<Individual> {
+
+	@Override
+	public int compare(Individual i1, Individual i2) {
+
+		String n1 = "-unknown-";
+		if (i1.names.size() > 0 && i1.names.get(0) != null) {
+			n1 = i1.names.get(0).toString();
+		}
+		String n2 = "-unknown-";
+		if (i2.names.size() > 0 && i2.names.get(0) != null) {
+			n2 = i2.names.get(0).toString();
+		}
+
+		return n1.compareTo(n2);
+	}
+
+}
