a699cec17c9243d6d122bdb407e6cd43fcbd856d
Matt Harrah (frizbog) <frizbog1@gmail.com>
2012-12-11 20:06:14 -0500

    v2.1.0 - includes custom tag support, removes use of temp files, and fixes the lastname-first comparator.

--- test/org/gedcom4j/comparators/IndividualByLastNameFirstNameComparatorTest.java
+++ test/org/gedcom4j/comparators/IndividualByLastNameFirstNameComparatorTest.java
@@ -23,12 +23,10 @@ package org.gedcom4j.comparators;
 
 import static org.junit.Assert.assertTrue;
 
-import org.gedcom4j.comparators.IndividualByLastNameFirstNameComparator;
 import org.gedcom4j.model.Individual;
 import org.gedcom4j.model.PersonalName;
 import org.junit.Before;
 import org.junit.Test;
-
 
 /**
  * Test for {@link IndividualByLastNameFirstNameComparator}
@@ -42,122 +40,16 @@ public class IndividualByLastNameFirstNameComparatorTest {
      * The comparator being tested
      */
     IndividualByLastNameFirstNameComparator c;
+
     /**
      * Test fixture - one individual
      */
     Individual i1;
+
     /**
      * Test fixture - second individual
      */
     Individual i2;
-
-    /**
-     * Set up test fixtures
-     */
-    @Before
-    public void setUp() {
-        c = new IndividualByLastNameFirstNameComparator();
-        i1 = new Individual();
-        i2 = new Individual();
-    }
-
-    /**
-     * Test for
-     * {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)}
-     * . Tests when both individuals have multiple names.
-     */
-    @Test
-    public void testBothHaveMultipleNames() {
-        addBasicName(i1, "Bob /Martin/");
-        addBasicName(i1, "Aaron /Aardvark/");
-        addBasicName(i2, "Bob /Marley/");
-        addBasicName(i2, "Ziggy /Zoots/");
-
-        assertTrue(
-                "Bob Martin comes after Bob Marley, and Ziggy Zoots and Aaron Aardvark get ignored",
-                c.compare(i1, i2) > 0);
-        assertTrue(
-                "Bob Marley comes before Bob Martin, and Ziggy Zoots and Aaron Aardvark get ignored",
-                c.compare(i2, i1) < 0);
-    }
-
-    /**
-     * Test for
-     * {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)}
-     * . Tests when neither individual has any names.
-     */
-    @Test
-    public void testNobodyHasNames() {
-        assertTrue("Individuals with no names should compare equal",
-                c.compare(i1, i2) == 0);
-        assertTrue("Individuals with no names should compare equal",
-                c.compare(i2, i1) == 0);
-    }
-
-    /**
-     * Test for
-     * {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)}
-     * . Tests when one individual has multiple names.
-     */
-    @Test
-    public void testOneHasMultipleNames() {
-        addBasicName(i1, "Bob /Martin/");
-        addBasicName(i2, "Bob /Marley/");
-        addBasicName(i2, "Ziggy /Zoots/");
-
-        assertTrue(
-                "Bob Martin comes after Bob Marley, and Ziggy Zoots gets ignored",
-                c.compare(i1, i2) > 0);
-        assertTrue(
-                "Bob Marley comes before Bob Martin, and Ziggy Zoots gets ignored",
-                c.compare(i2, i1) < 0);
-    }
-
-    /**
-     * Test for
-     * {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)}
-     * . Tests when one individual has no names and the other does
-     */
-    @Test
-    public void testOneHasNoName() {
-        addBasicName(i1, "Bob /Marley/");
-
-        assertTrue(
-                "The individual with a name should always come after the individual with no names",
-                c.compare(i1, i2) > 0);
-        assertTrue(
-                "The individual with a name should always come after the individual with no names",
-                c.compare(i2, i1) < 0);
-    }
-
-    /**
-     * Test for
-     * {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)}
-     * . Tests when last names differ.
-     */
-    @Test
-    public void testSurnamesDiffer() {
-        addBasicName(i1, "Bob /Martin/");
-        addBasicName(i2, "Bob /Marley/");
-
-        assertTrue("Bob Martin comes after Bob Marley", c.compare(i1, i2) > 0);
-        assertTrue("Bob Marley comes before Bob Martin", c.compare(i2, i1) < 0);
-    }
-
-    /**
-     * Test for
-     * {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)}
-     * . Tests when last names differ.
-     */
-    @Test
-    public void testSurnamesSame() {
-        addBasicName(i1, "Ziggy /Marley/");
-        addBasicName(i2, "Bob /Marley/");
-
-        assertTrue("Ziggy Marley comes after Bob Marley", c.compare(i1, i2) > 0);
-        assertTrue("Bob Marley comes before Ziggy Marley",
-                c.compare(i2, i1) < 0);
-    }
 
     /**
      * Helper method for readability, that adds a basic name to an individual
@@ -171,6 +63,97 @@ public class IndividualByLastNameFirstNameComparatorTest {
         PersonalName n = new PersonalName();
         n.basic = string;
         i.names.add(n);
+    }
+
+    /**
+     * Set up test fixtures
+     */
+    @Before
+    public void setUp() {
+        c = new IndividualByLastNameFirstNameComparator();
+        i1 = new Individual();
+        i2 = new Individual();
+    }
+
+    /**
+     * Test for {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)} . Tests when both
+     * individuals have multiple names.
+     */
+    @Test
+    public void testBothHaveMultipleNames() {
+        addBasicName(i1, "Bob /Martin/");
+        addBasicName(i1, "Aaron /Aardvark/");
+        addBasicName(i2, "Bob /Marley/");
+        addBasicName(i2, "Ziggy /Zoots/");
+
+        assertTrue("Bob Martin comes after Bob Marley, and Ziggy Zoots and Aaron Aardvark get ignored",
+                c.compare(i1, i2) > 0);
+        assertTrue("Bob Marley comes before Bob Martin, and Ziggy Zoots and Aaron Aardvark get ignored",
+                c.compare(i2, i1) < 0);
+    }
+
+    /**
+     * Test for {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)} . Tests when neither
+     * individual has any names.
+     */
+    @Test
+    public void testNobodyHasNames() {
+        assertTrue("Individuals with no names should compare equal", c.compare(i1, i2) == 0);
+        assertTrue("Individuals with no names should compare equal", c.compare(i2, i1) == 0);
+    }
+
+    /**
+     * Test for {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)} . Tests when one
+     * individual has multiple names.
+     */
+    @Test
+    public void testOneHasMultipleNames() {
+        addBasicName(i1, "Bob /Martin/");
+        addBasicName(i2, "Bob /Marley/");
+        addBasicName(i2, "Ziggy /Zoots/");
+
+        assertTrue("Bob Martin comes after Bob Marley, and Ziggy Zoots gets ignored", c.compare(i1, i2) > 0);
+        assertTrue("Bob Marley comes before Bob Martin, and Ziggy Zoots gets ignored", c.compare(i2, i1) < 0);
+    }
+
+    /**
+     * Test for {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)} . Tests when one
+     * individual has no names and the other does
+     */
+    @Test
+    public void testOneHasNoName() {
+        addBasicName(i1, "Bob /Marley/");
+
+        assertTrue("The individual with a name should always come after the individual with no names",
+                c.compare(i1, i2) > 0);
+        assertTrue("The individual with a name should always come after the individual with no names",
+                c.compare(i2, i1) < 0);
+    }
+
+    /**
+     * Test for {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)} . Tests when last names
+     * differ.
+     */
+    @Test
+    public void testSurnamesDiffer() {
+        addBasicName(i1, "Bob /Martin/");
+        addBasicName(i2, "Bob /Marley/");
+
+        assertTrue("Bob Martin comes after Bob Marley", c.compare(i1, i2) > 0);
+        assertTrue("Bob Marley comes before Bob Martin", c.compare(i2, i1) < 0);
+    }
+
+    /**
+     * Test for {@link IndividualByLastNameFirstNameComparator#compare(Individual, Individual)} . Tests when last names
+     * differ.
+     */
+    @Test
+    public void testSurnamesSame() {
+        addBasicName(i1, "Ziggy /Marley/");
+        addBasicName(i2, "Bob /Marley/");
+
+        assertTrue("Ziggy Marley comes after Bob Marley", c.compare(i1, i2) > 0);
+        assertTrue("Bob Marley comes before Ziggy Marley", c.compare(i2, i1) < 0);
     }
 
 }
