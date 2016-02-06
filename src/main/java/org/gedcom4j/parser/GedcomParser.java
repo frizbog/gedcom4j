60feb170b4ed09f8b5d3eb070a7b90053ba48c91
Matt Harrah (frizbog) <frizbog1@gmail.com>
2016-02-05 19:14:02 -0500

    Updated copyright notice.

--- src/main/java/org/gedcom4j/parser/GedcomParser.java
+++ src/main/java/org/gedcom4j/parser/GedcomParser.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2009-2014 Matthew R. Harrah
+ * Copyright (c) 2009-2016 Matthew R. Harrah
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- src/test/java/org/gedcom4j/model/LdsSpouseSealingTest.java
+++ src/test/java/org/gedcom4j/model/LdsSpouseSealingTest.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2009-2016 Matthew R. Harrah
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.gedcom4j.model;
 
 import static org.junit.Assert.assertEquals;
@@ -93,9 +114,7 @@ public class LdsSpouseSealingTest {
     @Test
     public void testToString() {
         LdsSpouseSealing l = new LdsSpouseSealing();
-        assertEquals(
-                "LdsSpouseSealing [status=null, date=null, temple=null, place=null, citations=[], notes=[], customTags=[]]",
-                l.toString());
+        assertEquals("LdsSpouseSealing [status=null, date=null, temple=null, place=null, citations=[], notes=[], customTags=[]]", l.toString());
 
         l.citations.add(new CitationWithoutSource());
         l.customTags.add(new StringTree());
