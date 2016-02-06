01da670570ad0eba60cbedd3a8bc85caa76caa49
Matt Harrah (frizbog) <frizbog1@gmail.com>
2016-02-05 19:14:02 -0500

    Updated copyright notice.

--- src/main/java/org/gedcom4j/relationship/SimplificationRules.java
+++ src/main/java/org/gedcom4j/relationship/SimplificationRules.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2009-2016 Matthew R. Harrah
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,38 +30,6 @@ import java.util.List;
  * A class that holds rules for simplifying complex relationships into shorter ones
  */
 final class SimplificationRules {
-
-    /**
-     * <p>
-     * A list of 3-element arrays, containing rules on how to collapse complicated relationships down into simpler
-     * forms. Each row is an array of three {@link RelationshipName}s; let's call them r1, r2, and r3. Let's also assume
-     * there are three people: A, B, and C. If A is related to B by r1, and B is related to C by r2, then A is related
-     * to C as r3 and B can be removed.
-     * </p>
-     * <p>
-     * Example: Rule= <code>[FATHER, SON, BROTHER]</code>. Bob's FATHER is Jim; Jim's SON is Fred; therefore Bob's
-     * BROTHER is Fred.
-     * </p>
-     * <p>
-     * The order that these triplets appear in the list is the order in which the rules are applied. Rules that build on
-     * previous relationship simplifications should appear later in the list.
-     * </p>
-     */
-    static List<RelationshipName[]> rules = new ArrayList<RelationshipName[]>();
-
-    /* static initializer to load the list */
-    static {
-        mothersAndFathers();
-        siblings();
-        grandparentsAndGrandchildren();
-        greatGrandparentsAndGreatGrandchildren();
-        greatGreatGrandparentsAndGreatGreatGrandchildren();
-        greatGreatGreatGrandparentsAndGreatGreatGreatGrandchildren();
-        auntsUnclesNiecesNephews();
-        firstCousins();
-        greatAuntsUnclesNiecesNephews();
-        greatGreatAuntsUnclesNiecesNephews();
-    }
 
     /**
      * Load the rules for aunts, uncles, nieces, and nephews
@@ -344,6 +313,38 @@ final class SimplificationRules {
         newRule(MOTHER, DAUGHTER, SISTER);
         newRule(FATHER, CHILD, SIBLING);
         newRule(MOTHER, CHILD, SIBLING);
+    }
+
+    /**
+     * <p>
+     * A list of 3-element arrays, containing rules on how to collapse complicated relationships down into simpler
+     * forms. Each row is an array of three {@link RelationshipName}s; let's call them r1, r2, and r3. Let's also assume
+     * there are three people: A, B, and C. If A is related to B by r1, and B is related to C by r2, then A is related
+     * to C as r3 and B can be removed.
+     * </p>
+     * <p>
+     * Example: Rule= <code>[FATHER, SON, BROTHER]</code>. Bob's FATHER is Jim; Jim's SON is Fred; therefore Bob's
+     * BROTHER is Fred.
+     * </p>
+     * <p>
+     * The order that these triplets appear in the list is the order in which the rules are applied. Rules that build on
+     * previous relationship simplifications should appear later in the list.
+     * </p>
+     */
+    static List<RelationshipName[]> rules = new ArrayList<RelationshipName[]>();
+
+    /* static initializer to load the list */
+    static {
+        mothersAndFathers();
+        siblings();
+        grandparentsAndGrandchildren();
+        greatGrandparentsAndGreatGrandchildren();
+        greatGreatGrandparentsAndGreatGreatGrandchildren();
+        greatGreatGreatGrandparentsAndGreatGreatGreatGrandchildren();
+        auntsUnclesNiecesNephews();
+        firstCousins();
+        greatAuntsUnclesNiecesNephews();
+        greatGreatAuntsUnclesNiecesNephews();
     }
 
     /**
--- src/test/java/org/gedcom4j/model/FamilyEventTypeTest.java
+++ src/test/java/org/gedcom4j/model/FamilyEventTypeTest.java
@@ -1,6 +1,25 @@
+/*
+ * Copyright (c) 2009-2016 Matthew R. Harrah
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.gedcom4j.model;
-
-import org.gedcom4j.model.FamilyEventType;
 
 import junit.framework.TestCase;
 
@@ -15,23 +34,16 @@ public class FamilyEventTypeTest extends TestCase {
      * Test for {@link FamilyEventType#getFromTag(String)}
      */
     public void testGetFromTag() {
-        assertSame(FamilyEventType.ANNULMENT,
-                FamilyEventType.getFromTag("ANUL"));
+        assertSame(FamilyEventType.ANNULMENT, FamilyEventType.getFromTag("ANUL"));
         assertSame(FamilyEventType.CENSUS, FamilyEventType.getFromTag("CENS"));
         assertSame(FamilyEventType.DIVORCE, FamilyEventType.getFromTag("DIV"));
-        assertSame(FamilyEventType.DIVORCE_FILED,
-                FamilyEventType.getFromTag("DIVF"));
-        assertSame(FamilyEventType.ENGAGEMENT,
-                FamilyEventType.getFromTag("ENGA"));
+        assertSame(FamilyEventType.DIVORCE_FILED, FamilyEventType.getFromTag("DIVF"));
+        assertSame(FamilyEventType.ENGAGEMENT, FamilyEventType.getFromTag("ENGA"));
         assertSame(FamilyEventType.MARRIAGE, FamilyEventType.getFromTag("MARR"));
-        assertSame(FamilyEventType.MARRIAGE_BANNER,
-                FamilyEventType.getFromTag("MARB"));
-        assertSame(FamilyEventType.MARRIAGE_CONTRACT,
-                FamilyEventType.getFromTag("MARC"));
-        assertSame(FamilyEventType.MARRIAGE_LICENSE,
-                FamilyEventType.getFromTag("MARL"));
-        assertSame(FamilyEventType.MARRIAGE_SETTLEMENT,
-                FamilyEventType.getFromTag("MARS"));
+        assertSame(FamilyEventType.MARRIAGE_BANNER, FamilyEventType.getFromTag("MARB"));
+        assertSame(FamilyEventType.MARRIAGE_CONTRACT, FamilyEventType.getFromTag("MARC"));
+        assertSame(FamilyEventType.MARRIAGE_LICENSE, FamilyEventType.getFromTag("MARL"));
+        assertSame(FamilyEventType.MARRIAGE_SETTLEMENT, FamilyEventType.getFromTag("MARS"));
         assertSame(FamilyEventType.EVENT, FamilyEventType.getFromTag("EVEN"));
         assertNull(FamilyEventType.getFromTag("BAD"));
         assertNull(FamilyEventType.getFromTag(null));
