aaddd4ccfd819f9702d9ff0ce3234f33c6deed8e
Matt Harrah (frizbog) <frizbog1@gmail.com>
2016-06-27 15:30:52 -0400

    More work on Issue #105 - adding listeners and ability to cancel parse/write operations.

---
+++ src/main/java/org/gedcom4j/parser/event/ParseProgressEvent.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2009-2016 Matthew R. Harrah
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation parses (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.gedcom4j.parser.event;
+
+import java.io.Serializable;
+import java.util.EventObject;
+
+import org.gedcom4j.model.Gedcom;
+
+/**
+ * An event to hold information about parsing progress.
+ * 
+ * @author frizbog
+ */
+public class ParseProgressEvent extends EventObject implements Serializable {
+
+    /**
+     * Serial version uid
+     */
+    private static final long serialVersionUID = 996714620347714206L;
+
+    /**
+     * Is the parse completely processed?
+     */
+    private final boolean complete;
+
+    /**
+     * Number of families processed
+     */
+    private final int familiesProcessed;
+
+    /**
+     * Number of individuals processed
+     */
+    private final int individualsProcessed;
+
+    /**
+     * Number of multimedia processed
+     */
+    private final int multimediaProcessed;
+
+    /**
+     * Number of notes processed
+     */
+    private final int notesProcessed;
+
+    /**
+     * Number of families processed
+     */
+    private final int repositoriesProcessed;
+
+    /**
+     * Number of sources processed
+     */
+    private final int sourcesProcessed;
+
+    /**
+     * Number of submitters processed
+     */
+    private final int submittersProcessed;
+
+    /**
+     * Constructor
+     * 
+     * @param source
+     *            the source object
+     * @param g
+     *            the gedcom being loaded - this is used for the source of the progress data
+     * @param complete
+     *            is the parse complete
+     */
+    public ParseProgressEvent(Object source, Gedcom g, boolean complete) {
+        super(source);
+        familiesProcessed = g.families.size();
+        individualsProcessed = g.individuals.size();
+        multimediaProcessed = g.multimedia.size();
+        notesProcessed = g.notes.size();
+        repositoriesProcessed = g.repositories.size();
+        sourcesProcessed = g.sources.size();
+        submittersProcessed = g.submitters.size();
+        this.complete = complete;
+    }
+
+    /**
+     * Get number of families processed
+     * 
+     * @return number of families processed
+     */
+    public int getFamiliesProcessed() {
+        return familiesProcessed;
+    }
+
+    /**
+     * Get number of individuals processed
+     * 
+     * @return number of individuals processed
+     */
+    public int getIndividualsProcessed() {
+        return individualsProcessed;
+    }
+
+    /**
+     * Get number of multimedia processed
+     * 
+     * @return number of multimedia processed
+     */
+    public int getMultimediaProcessed() {
+        return multimediaProcessed;
+    }
+
+    /**
+     * Get number of notes processed
+     * 
+     * @return number of notes processed
+     */
+    public int getNotesProcessed() {
+        return notesProcessed;
+    }
+
+    /**
+     * Get number of repositories processed
+     * 
+     * @return number of repositories processed
+     */
+    public int getRepositoriesProcessed() {
+        return repositoriesProcessed;
+    }
+
+    /**
+     * Get number of sources processed
+     * 
+     * @return number of sources processed
+     */
+    public int getSourcesProcessed() {
+        return sourcesProcessed;
+    }
+
+    /**
+     * Get number of submitters processed
+     * 
+     * @return number of submitters processed
+     */
+    public int getSubmittersProcessed() {
+        return submittersProcessed;
+    }
+
+    /**
+     * Is the parse operation complete?
+     * 
+     * @return true if the pars operation is complete
+     */
+    public boolean isComplete() {
+        return complete;
+    }
+
+    @Override
+    public String toString() {
+        return "ParseProgressEvent [complete=" + complete + ", familiesProcessed=" + familiesProcessed + ", individualsProcessed=" + individualsProcessed
+                + ", multimediaProcessed=" + multimediaProcessed + ", notesProcessed=" + notesProcessed + ", repositoriesProcessed=" + repositoriesProcessed
+                + ", sourcesProcessed=" + sourcesProcessed + ", submittersProcessed=" + submittersProcessed + "]";
+    }
+
+}
---
+++ src/test/java/org/gedcom4j/parser/ParserProgressAndCancellationTest.java
@@ -0,0 +1,92 @@
+/* 
+ * Copyright (c) 2009-2016 Matthew R. Harrah
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.gedcom4j.parser;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.gedcom4j.io.exception.LoadCancelledException;
+import org.gedcom4j.parser.event.ParseProgressEvent;
+import org.gedcom4j.parser.event.ParseProgressListener;
+import org.junit.Test;
+
+/**
+ * Test getting progress from the parser and being able to cancel
+ * 
+ * @author frizbog
+ */
+public class ParserProgressAndCancellationTest implements ParseProgressListener {
+
+    /**
+     * Number of notifications received
+     */
+    int notificationCount = 0;
+
+    /**
+     * The parser being tested
+     */
+    private GedcomParser gp = new GedcomParser();
+
+    /**
+     * How many notifications to cancel after
+     */
+    private int cancelAfter = 0;
+
+    @Override
+    public void progressNotification(ParseProgressEvent e) {
+        notificationCount++;
+        if (notificationCount >= cancelAfter) {
+            gp.cancel();
+        }
+    }
+
+    /**
+     * Test getting notifications and cancelling the parsing of an ascii file
+     * 
+     * @throws IOException
+     * @throws GedcomParserException
+     */
+    @Test(expected = LoadCancelledException.class)
+    public void testCancellation() throws IOException, GedcomParserException {
+        gp = new GedcomParser();
+        cancelAfter = 100;
+        gp.registerParseObserver(this);
+        gp.load("sample/willis-ascii.ged");
+    }
+
+    /**
+     * Test getting notifications and cancelling the parsing of an ascii file
+     * 
+     * @throws IOException
+     * @throws GedcomParserException
+     */
+    @Test
+    public void testNoCancellation() throws IOException, GedcomParserException {
+        gp = new GedcomParser();
+        cancelAfter = Integer.MAX_VALUE;
+        gp.registerParseObserver(this);
+        gp.load("sample/willis-ascii.ged");
+        assertEquals(1269, notificationCount);
+    }
+
+}
