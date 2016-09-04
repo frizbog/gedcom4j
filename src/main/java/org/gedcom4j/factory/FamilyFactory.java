c3c2bad30b8b1005d51cfa3b910bc55599fa4a51
Matt Harrah (frizbog) <frizbog1@gmail.com>
2016-09-03 23:12:13 -0400

    Issue #123 - New class/method to create an family

---
+++ src/main/java/org/gedcom4j/factory/FamilyFactory.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2009-2016 Matthew R. Harrah
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.gedcom4j.factory;
+
+import org.gedcom4j.model.Family;
+import org.gedcom4j.model.FamilyChild;
+import org.gedcom4j.model.FamilySpouse;
+import org.gedcom4j.model.Gedcom;
+import org.gedcom4j.model.Individual;
+
+import edu.emory.mathcs.backport.java.util.Arrays;
+
+/**
+ * Class to build a {@link Family} object
+ * 
+ * @author frizbog
+ */
+public class FamilyFactory {
+
+    /**
+     * Create a family, add people to it, and put it in the gedcom
+     * 
+     * @param g
+     *            the gedcom
+     * @param father
+     *            the father - optional, but if supplied, must already exist in the gedcom (by xref)
+     * @param mother
+     *            the mother - optional, but if supplied, must already exist in the gedcom (by xref)
+     * @param children
+     *            the children - optional, but if supplied, must already exist in the gedcom (by xref)
+     * @return the family created and added to the gedcom
+     */
+    public Family create(Gedcom g, Individual father, Individual mother, Individual... children) {
+
+        // Validate that the people passed in exist
+        if (father != null && !g.getIndividuals().containsKey(father.getXref())) {
+            throw new IllegalArgumentException("Father could not be found by xref in supplied gedcom object: " + father.getXref());
+        }
+        if (mother != null && !g.getIndividuals().containsKey(mother.getXref())) {
+            throw new IllegalArgumentException("Mother could not be found by xref in supplied gedcom object: " + mother.getXref());
+        }
+        if (children != null) {
+            for (Individual kid : children) {
+                if (!g.getIndividuals().containsKey(kid.getXref())) {
+                    throw new IllegalArgumentException("Child could not be found by xref in supplied gedcom object: " + kid
+                            .getXref());
+                }
+            }
+        }
+
+        Family result = new Family();
+
+        // Make an xref and add to gedcom
+        for (int xref = g.getFamilies().size(); !g.getFamilies().containsKey("@F" + xref + "@") && result
+                .getXref() == null; xref++) {
+            result.setXref("@F" + xref + "@");
+            g.getFamilies().put(result.getXref(), result);
+        }
+
+        // Put the people in the Family record
+        result.setHusband(father);
+        result.setWife(mother);
+        result.getChildren(true).addAll(Arrays.asList(children));
+
+        // And add the family record to the people
+        if (father != null) {
+            FamilySpouse fams = new FamilySpouse();
+            fams.setFamily(result);
+            father.getFamiliesWhereSpouse(true).add(fams);
+        }
+        if (mother != null) {
+            FamilySpouse fams = new FamilySpouse();
+            fams.setFamily(result);
+            mother.getFamiliesWhereSpouse(true).add(fams);
+        }
+        if (children != null) {
+            for (Individual kid : children) {
+                FamilyChild famc = new FamilyChild();
+                famc.setFamily(result);
+                kid.getFamiliesWhereChild(true).add(famc);
+            }
+        }
+
+        return result;
+    }
+
+}
---
+++ src/test/java/org/gedcom4j/factory/FamilyFactoryTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2009-2016 Matthew R. Harrah
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.gedcom4j.factory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.gedcom4j.model.Family;
+import org.gedcom4j.model.Gedcom;
+import org.gedcom4j.model.Individual;
+import org.junit.Test;
+
+/**
+ * Test for {@link FamilyFactory}
+ * 
+ * @author frizbog
+ *
+ */
+public class FamilyFactoryTest {
+
+    /**
+     * Test for {@link FamilyFactory#create(Gedcom, Individual, Individual, Individual...)}
+     */
+    @SuppressWarnings("deprecation")
+    @Test
+    public void test() {
+        Gedcom g = new Gedcom();
+        Individual father = new IndividualFactory().create(g, "Robert", "Tarantino", Sex.MALE, new Date(67, Calendar.MAY, 1),
+                "Idaho", new Date(99, Calendar.OCTOBER, 31), "Virginia");
+        Individual mother = new IndividualFactory().create(g, "Theresa", "Guliani", Sex.FEMALE, new Date(68, Calendar.SEPTEMBER,
+                15), "Idaho", null, null);
+        Individual kid1 = new IndividualFactory().create(g, "Bernardo", "Tarantino", Sex.MALE, new Date(93, Calendar.APRIL, 5),
+                "Idaho", null, null);
+        Individual kid2 = new IndividualFactory().create(g, "Angela", "Tarantino", Sex.MALE, new Date(95, Calendar.DECEMBER, 8),
+                "Idaho", null, null);
+
+        Family f = new FamilyFactory().create(g, father, mother, kid1, kid2);
+
+        assertNotNull(f);
+        assertNotNull(f.getXref());
+
+        Family f2 = g.getFamilies().get(f.getXref());
+        assertSame(f2, f);
+
+        assertSame(father, f.getHusband());
+        assertSame(mother, f.getWife());
+        assertTrue(f.getChildren().contains(kid1));
+        assertTrue(f.getChildren().contains(kid2));
+        assertEquals(2, f.getChildren().size());
+
+        assertSame(father.getFamiliesWhereSpouse().get(0).getFamily(), f);
+        assertSame(mother.getFamiliesWhereSpouse().get(0).getFamily(), f);
+        assertSame(kid1.getFamiliesWhereChild().get(0).getFamily(), f);
+        assertSame(kid2.getFamiliesWhereChild().get(0).getFamily(), f);
+    }
+
+}
