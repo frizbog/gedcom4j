4f1db7179f384e29dcc2367250aa1609a1669633
Matt Harrah (frizbog) <frizbog1@gmail.com>
2016-09-29 07:55:36 -0400

    Issue #166

---
+++ src/test/java/org/gedcom4j/parser/Issue166Test.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2009-2016 Matthew R. Harrah
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.gedcom4j.parser;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import org.gedcom4j.exception.GedcomParserException;
+import org.gedcom4j.model.Gedcom;
+import org.gedcom4j.model.Individual;
+import org.junit.Test;
+
+/**
+ * Test for issue #166 where indented files with leading whitespace on lines (such as those exported by GENi.com) were not parsing.
+ * 
+ * @author frizbog
+ */
+public class Issue166Test {
+
+    /**
+     * Test loading a submitted file that came with the issue report
+     * 
+     * @throws GedcomParserException
+     *             if the file cannot be parsed
+     * @throws IOException
+     *             if the file cannot be read
+     */
+    @Test
+    public void testIndentedLines() throws IOException, GedcomParserException {
+        GedcomParser gp = new GedcomParser();
+        gp.load("sample/Issue166.ged");
+        Gedcom g = gp.getGedcom();
+        assertNotNull(g);
+        Individual i = g.getIndividuals().get("@I6000000047602801251@");
+        assertNotNull(i);
+        assertNotNull(i.getNames());
+    }
+
+}
